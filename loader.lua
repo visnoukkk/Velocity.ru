8283963c7cf93b67e78444e32c4430cb488c111c
visnoukkk <chamnanchanvathana@gmail.com>
2026-09-12 18:43:32 +0700

    Remove conditional loading for Lua script

--- loader.lua
+++ loader.lua
@@ -1,3 +1,1 @@
-if game.PlaceId == 17625359962 then
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/d5000d1a62432d8e4b762fecbe49cf66.lua"))()
-end
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/d5000d1a62432d8e4b762fecbe49cf66.lua"))()
